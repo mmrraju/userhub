67fe3aa04aeacb4d307a031751c6d35f2704f995
mmrraju <mrraju.ice.iu@gmail.com>
2025-05-13 12:16:41 +0600

    Initial commit: Spring Boot UserHub project

---
+++ src/main/java/com/fusionpulse/userhub/UserHubApplication.java
@@ -0,0 +1,13 @@
+package com.fusionpulse.userhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHubApplication.class, args);
+	}
+
+}
